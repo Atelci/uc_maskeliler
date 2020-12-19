c0b7fce5ec630c6b9521d14726fae328b397b0a3
atelci <atamant@netrd.com.tr>
2020-12-19 20:21:58 +0300

    initial UI

--- oraas_client/src/App.js
+++ oraas_client/src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header></Header>
+        <div className="App-bg">
+        </div>
+        <div className="App-body">
+
+          <div className="transbox-left">
+            <ul>
+              <li>
+                <label htmlFor="userName" className="form-label">Your Name:</label>
+                <input type="text" class="form-control" id="userName" placeholder="John"/>
+              </li>
+
+              <li>
+                <label htmlFor="classSelected" className="form-label">Choose class:</label>
+                <select className="selectpicker">
+                  <optgroup label="Offical">
+                    <option>YoloV3</option>
+                  </optgroup>
+                  <hr></hr>
+                  <optgroup label="Custom">
+                    <option>Mask</option>
+                  </optgroup>
+                </select>
+              </li>
+
+              <li>
+                <label className="form-label" htmlFor="customFile">Default file input example:</label>
+                <img src={process.env.PUBLIC_URL + '/example-image.jpg'} alt="example"></img>
+              </li>
+
+              <li>
+                <label className="form-label">Your File:</label>
+                <input type="file" className="form-control" id="customFile" />
+              </li>
+
+            </ul>
+          </div>
+
+          <div className="transition">
+            <a href="#">Start Processing &raquo;&raquo;</a>
+          </div>
+
+          <div className="transbox-right">
+            <ul className="bulletless">
+              <li>
+                <label className="form-label">Default file output example:</label>
+              </li>
+              <li>
+                <img src={process.env.PUBLIC_URL + '/example-image.jpg'} alt="example"></img>
+              </li>
+              <hr></hr>
+              <li>
+                <p>Processing your image...</p>
+                <img src={process.env.PUBLIC_URL + '/loading.gif'} alt="loading"></img>
+                <button type="button" className="btn btn-primary" disabled>Download</button>
+              </li>
+            </ul>
+          </div>
+
+        </div>
     </div>
   );
 }
---
+++ oraas_client/src/components/header.js
@@ -0,0 +1,28 @@
+import '../App.css';
+
+function Header() {
+    return (
+        <>
+            <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+                <div className="collapse navbar-collapse" id="navbarsExampleDefault">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <h3 className="navbar-brand">3 Maskeliler</h3>
+                        </li>
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Class List</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">My Image</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </>
+    );
+}
+
+export default Header;
